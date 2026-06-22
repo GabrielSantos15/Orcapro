8a050ee5f780412b0a3581f968e8a73b4669db15
Gabriel dos Santos <gs303291@gmail.com>
2026-06-22 00:26:06 -0300

    chore(orcamento): muda o formato da resposta do endpoit de orcamento

--- backend/src/main/java/br/com/fiap/orcapro/dto/OrcamentoResponseDTO.java
+++ backend/src/main/java/br/com/fiap/orcapro/dto/OrcamentoResponseDTO.java
@@ -7,15 +7,13 @@ import java.math.BigDecimal;
 public record OrcamentoResponseDTO(
         Long id,
         BigDecimal limite,
-        Long categoriaId,
-        String categoriaNome
+        CategoriaResumoDTO categoria
 ) {
     public OrcamentoResponseDTO(Orcamento orcamento) {
         this(
                 orcamento.getId(),
                 orcamento.getLimite(),
-                orcamento.getCategoria().getId(),
-                orcamento.getCategoria().getNome()
+                new CategoriaResumoDTO(orcamento.getCategoria())
         );
     }
 }
